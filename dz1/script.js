0b5af08c185b7db61b1e2c15bae1d3ae5d1b7438
Jasu1bek20 <159901499+Jasu1bek20@users.noreply.github.com>
2024-02-27 17:04:42 +0600

    Add files via upload
    sdfghjkl

---
+++ dz1/script.js
@@ -0,0 +1,15 @@
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  function openSecondModal() {
+    document.getElementById("secondModal").style.display = "block";
+  }
+  
+  function closeSecondModal() {
+    document.getElementById("secondModal").style.display = "none";
+  }
